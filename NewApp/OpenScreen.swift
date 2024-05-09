0a62aa8d0fa3bc1c8aa48a46d91ea5096cc922d7
Nick <nickauerbach42@gmail.com>
2024-05-09 09:58:35 -0700

    Day one done

--- NewApp/OpenScreen.swift
+++ NewApp/OpenScreen.swift
@@ -8,19 +8,35 @@
 import SwiftUI
 
 struct OpenScreen: View {
+    
+    private var colors: [Color] = [.red, .green, .blue, .purple, .pink, .yellow, .cyan]
+    private var words = ["hi", "bye", "APP", "Nick"]
+    @State private var current = "APP"
     var body: some View {
-        Text("APP")
-            .font(.system(size:50))
+        VStack(spacing: 60){
+            Text(current)
+                .font(.system(size:50))
             
-        Circle()
-            .fill(Color.red)
-            .padding(20)
-            .overlay(
-                Text("Click To Start")
-                    .bold()
-                    .font(.system(size:30))
-                    .foregroundColor(.white)
-            )
+            Circle()
+                .fill(Color.red)
+                .padding(20)
+                .overlay(
+                    Text("Click To Start")
+                        .bold()
+                        .font(.system(size:30))
+                        .foregroundColor(.white)
+                    
+                )
+            Button("Click To Start"){
+                withAnimation {
+                    current = words.randomElement() ?? "APP"
+                    //curCol = colors.randomElement() ?? .red
+                    
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+        }
         
         
     }
