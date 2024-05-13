fc80047a836db938811005e042a8efb90e05585e
Nick <nickauerbach42@gmail.com>
2024-05-13 10:01:02 -0700

    Day Two
    nav between pages and pull content from internet

--- NewApp/NewAppApp.swift
+++ NewApp/NewAppApp.swift
@@ -12,7 +12,6 @@ struct NewAppApp: App {
     var body: some Scene {
         WindowGroup {
             OpenScreen();
-            ContentView().background(Color.red);
         }
     }
 }
--- NewApp/OpenScreen.swift
+++ NewApp/OpenScreen.swift
@@ -12,33 +12,42 @@ struct OpenScreen: View {
     private var colors: [Color] = [.red, .green, .blue, .purple, .pink, .yellow, .cyan]
     private var words = ["hi", "bye", "APP", "Nick"]
     @State private var current = "APP"
+    @State private var update = Color.red
+    @State private var id = 1
     var body: some View {
-        VStack(spacing: 60){
-            Text(current)
-                .font(.system(size:50))
-            
-            Circle()
-                .fill(Color.red)
-                .padding(20)
-                .overlay(
-                    Text("Click To Start")
-                        .bold()
-                        .font(.system(size:30))
-                        .foregroundColor(.white)
-                    
-                )
-            Button("Click To Start"){
-                withAnimation {
-                    current = words.randomElement() ?? "APP"
-                    //curCol = colors.randomElement() ?? .red
-                    
-                }
+        NavigationStack{
+            NavigationLink("Click To Start"){
+                ContentView()
             }
-            .buttonStyle(.borderedProminent)
+            VStack(spacing: 60){
+                Text(current)
+                    .font(.system(size:50))
+                
+                Circle()
+                    .fill(update)
+                    .padding(20)
+                    .overlay(
+                        Button("Click To Start"){
+                        }
+                            .bold()
+                            .font(.system(size:30))
+                            .foregroundColor(.white)
+                        
+                    )
+                Button("Click To Start"){
+                    withAnimation {
+                        current = words.randomElement() ?? "APP"
+                        update = colors.randomElement() ?? .red
+                        id+=1
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                
+            }
+            .transition(.slide)
+            .id(id)
             
         }
-        
-        
     }
 }
 
